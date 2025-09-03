a01f1a1da871cad9519dcf4a808f1639cf6ba816
Rafael Rocha <rafaelsantoscv_@hotmail.com>
2025-09-02 22:59:23 -0300

    chore: configuração inicial da aplicação web

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+export function App() {
+
+  return (
+    <div className="grid min-h-screen grid-cols-2 antialiased">
+      <div className="bg-another-purple">
+        <h1>Proffy</h1>
+      </div>
+      
+      <div className="bg-background">
+        <h1>login</h1>
+      </div>
+    </div>
+  )
+}
